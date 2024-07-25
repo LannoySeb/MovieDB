818b9be732ecca69ee4ee32501739dba950a6639
SebLay <sebastien.lannoy98@gmail.com>
2024-07-25 14:52:35 +0200

    Very basic home setup

---
+++ app/src/main/java/com/example/moviedb/Feature/Home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.moviedb.Feature.Home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moviedb.databinding.FragmentHomeBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+
+    private val viewModel by viewModel<HomeViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
